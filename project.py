433530c802ed63b2398786ad6b25ea452f3d10ff
bhavya2903 <agarwalbhavya482@gmail.com>
2026-09-14 01:46:55 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,22 @@
+'''import random
+target = random.randint(1, 100)
+while True:
+    guess = int(input("Guess a number between 1 and 100: 0r quit(q) "))
+    if(target=="q"):
+        break
+    if guess < target:
+        print("Too low! Try again.")
+    elif guess > target:
+        print("Too high! Try again.")
+    else:
+        print("Congratulations! You guessed the number.")
+        break
+print("------game over------")'''
+import random
+import string
+password_length = 12
+characters = string.ascii_letters + string.digits + string.punctuation
+password=""
+for i in range(password_length):
+    password += random.choice(characters)
+print("Generated Password:", password)
